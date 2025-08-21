c8b5a5f89a954aaed15d4b1655c5191a082f57e4
Zain Ul Abideen <zainthenpc05@gmail.com>
2025-08-21 21:54:47 +0500

    Create connection.php

---
+++ hostel-allocation/db/connection.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'hostel_db';
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
